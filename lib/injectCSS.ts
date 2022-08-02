4a988b1875e373ffb3ec71f44a717378112c8c10
Kher Phay Chang <kayphay92@gmail.com>
2022-08-02 11:48:51 +0800

    fix(injectcss): fix server rendering bugs

--- lib/injectCSS.ts
+++ lib/injectCSS.ts
@@ -1,27 +1,26 @@
-
 /**
  * inject css to head
- * @param css 
+ * @param css
  */
 export function injectCSS(css: string, uniqueId: string, isPrepend?: boolean) {
-    if (!css || !document) {
-        return;
-    }
+	if (!css || !document) {
+		return;
+	}
 
-    const exists = !!document.querySelector(`style[id="${uniqueId}"]`)
-    if (exists) {
-        return;
-    }
+	const exists = !!document.querySelector(`style[id="${uniqueId}"]`);
+	if (exists) {
+		return;
+	}
 
-    const cssElement = document.createElement("style");
-    cssElement.id = uniqueId;
-    cssElement.setAttribute("type", "text/css");
+	const cssElement = document.createElement('style');
+	cssElement.id = uniqueId;
+	cssElement.setAttribute('type', 'text/css');
 
-    cssElement.innerHTML = css;
-    if (isPrepend && document.head.firstChild) {
-        document.head.insertBefore(cssElement, document.head.firstChild);
-    } else {
-        document.head.appendChild(cssElement);
-    }
-    return cssElement;
+	cssElement.innerHTML = css;
+	if (isPrepend && document.head.firstChild) {
+		document.head.insertBefore(cssElement, document.head.firstChild);
+	} else {
+		document.head.appendChild(cssElement);
+	}
+	return cssElement;
 }
